6879fa20661032b35065d946d1d406a30e8377bb
Joseph <frigidquery@gmail.com>
2023-08-09 08:14:28 -0600

    Stupid tower

---
+++ premade-theme-challenge/modules/sign-in.php
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="100%" height="100%" viewBox="0 0 4961 3508" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2;">
+    <rect x="692.153" y="1540.22" width="2060.22" height="734.478" style="fill:rgb(146,1,2);"/>
+    <g transform="matrix(8.20583e-17,1.34011,-1.50708,9.22822e-17,2813.09,503.934)">
+        <path d="M1047.32,941.204L1219.12,1277.63L875.519,1277.63L1047.32,941.204Z" style="fill:white;"/>
+    </g>
+    <g transform="matrix(1,0,0,1,100.757,35.6438)">
+        <g transform="matrix(266.667,0,0,266.667,2463.45,1953.94)">
+        </g>
+        <text x="1519.44px" y="1953.94px" style="font-family:'Roboto-Bold', 'Roboto';font-weight:700;font-size:266.667px;fill:white;">SIGN IN</text>
+    </g>
+</svg>
